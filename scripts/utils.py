daf1e5dab218ab7460d920ec3d76b9e2283cbed5
reecekeller <67561984+reecekeller@users.noreply.github.com>
2024-09-10 16:26:33 -0400

    organized code

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-from arena import Arena, Aperture
+from scripts.arena import Arena, Aperture
 from tqdm import tqdm 
 import numpy as np
 
@@ -100,13 +100,3 @@ def info_map(arena, circle1_center, circle2_center, aperture, radius):
 
             info_mat[i, j] = infoMetric(A1, A2) 
     return info_mat
-
-# Define the experiment setup
-arena = Arena(length=60, width=60, height=50)
-aperture = Aperture(arena_width=60, arena_height=50, arena_length=60, gap_width=10)
-circleL = (arena.width/2 - aperture.gap_width, arena.width, arena.height/2)
-circleR = (arena.width/2 + aperture.gap_width, arena.width, arena.height/2)
-
-info_mat = info_map(arena, circleL, circleR, aperture, radius=5)
-np.save(info_mat)
-print(info_mat)
